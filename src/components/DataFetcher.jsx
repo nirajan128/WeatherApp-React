7217e3423d8578ff4300bff66ca0d8fea515ec74
Nirajan <nirajanshrestha672@gmail.com>
2024-08-27 00:53:49 -0400

    added gitignore

--- src/components/DataFetcher.jsx
+++ src/components/DataFetcher.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, {useState, useEffect} from "react";
@@ -12,7 +13,7 @@ const options = {
     url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
     params: {q: 'Toronto', days: 30},
     headers: {
-      'x-rapidapi-key': '71d241f82emsh0188ff64afacc8fp121277jsnff7de149baf7',
+      'x-rapidapi-key': import.meta.env.VITE_WEATHER_API_KEY,
       'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
     }
   };
